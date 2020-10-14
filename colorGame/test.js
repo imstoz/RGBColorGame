329474d2c37bfca2dde750de7817e2decc0a8a22
imstoz <61974078+imstoz@users.noreply.github.com>
2020-10-14 18:17:02 +0100

    Add files via upload

---
+++ colorGame/test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const colorGame = () => {
+  return <View />;
+}
+
+export default colorGame;
